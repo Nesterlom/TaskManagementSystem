2ee8394789a329f38a2078de7782a9aaf64e0f6a
Nesterlom <nesterlobr@gmail.com>
2023-12-09 12:08:58 +0300

    Initial commit

---
+++ src/test/java/com/taskmanagementsystem/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanagementsystem.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
